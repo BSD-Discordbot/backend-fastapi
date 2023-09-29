a1ea952bc757d336c5c060b7454009997471a508
Legonzaur <legonzaur@live.fr>
2023-09-29 12:43:00 +0200

    initial migration

--- db/models.py
+++ db/models.py
@@ -7,10 +7,11 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 import datetime
 from typing import List
+from typing import Optional
 
 from .database import Base
 
-association_table = Table(
+player_has_cards = Table(
     "player_has_cards",
     Base.metadata,
     Column("player", ForeignKey("player.discord_id"), primary_key=True),
@@ -18,49 +19,62 @@ association_table = Table(
     Column("amount", Integer, primary_key=True),
 )
 
+card_has_tags = Table(
+    "card_has_tags",
+    Base.metadata,
+    Column("card", ForeignKey("card.id"), primary_key=True),
+    Column("tag", ForeignKey("tag.id"), primary_key=True),
+)
+
+event_has_cards = Table(
+    "event_has_cards",
+    Base.metadata,
+    Column("event", ForeignKey("event.id"), primary_key=True),
+    Column("card", ForeignKey("card.id"), primary_key=True),
+)
+
+class Tag(Base):
+    __tablename__ = "tag"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
+    name: Mapped[str] = mapped_column()
+
 class Card(Base):
     __tablename__ = "card"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
     name: Mapped[int] = mapped_column(index=True, unique=True)
-    rarity: Mapped[int] = mapped_column()
-
+    rarity: Mapped[int] = mapped_column(nullable=False)
+    tags: Mapped[List[Tag]] = relationship(secondary=card_has_tags)
+    upgrades: Mapped[List["CardUpgrade"]] = relationship(back_populates="card")
+    upgrade_requirements: Mapped[List["CardUpgrade"]] = relationship(back_populates="requirement")
+    
 class Player(Base):
     __tablename__ = "player"
-    discord_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    discord_id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
     balance: Mapped[int] = mapped_column(default=0)
     last_daily: Mapped[datetime.datetime] = mapped_column(DateTime)
     daily_streak: Mapped[int] = mapped_column(Integer, default=0)
-    cards: Mapped[List[Card]] = relationship(secondary=association_table)
+    cards: Mapped[List[Card]] = relationship(secondary=player_has_cards)
 
+class CardUpgrade(Base):
+    __tablename__ = "card_upgrade"
+    card_id: Mapped[int] = mapped_column(ForeignKey("card.id"), primary_key=True, index=True)
+    card: Mapped["Card"] = relationship(back_populates="upgrades")
+    amount: Mapped[int] = mapped_column(default=1)
+    requirement_id: Mapped[int] = mapped_column(ForeignKey("card.id"), primary_key=True)
+    requirement: Mapped["Card"] = relationship(back_populates="upgrade_requirements")
 
-# interface PlayerHasCard {
-#   discord_id: bigint
-#   card_id: number
-#   amount: number
-#   date_owned: Date
+# interface Event {
+#   id: Generated<number>
+#   start_time?: number
+#   end_time?: number
+#   name: string
+#   default: boolean
 # }
-
-    
-
-
-
-# class User(Base):
-#     __tablename__ = "users"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     email = Column(String, unique=True, index=True)
-#     hashed_password = Column(String)
-#     is_active = Column(Boolean, default=True)
-
-#     items = relationship("Item", back_populates="owner")
-
-
-# class Item(Base):
-#     __tablename__ = "items"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     title = Column(String, index=True)
-#     description = Column(String, index=True)
-#     owner_id = Column(Integer, ForeignKey("users.id"))
-
-#     owner = relationship("User", back_populates="items")
+class Event(Base):
+    __tablename__ = "event"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
+    start_time: Mapped[Optional[datetime.datetime]] = mapped_column()
+    end_time: Mapped[Optional[datetime.datetime]] = mapped_column()
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    default: Mapped[bool] = mapped_column(nullable=False, default=False)
+    cards: Mapped[List[Card]] = relationship(secondary=event_has_cards)
